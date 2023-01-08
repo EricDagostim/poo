fb3f765c8560436be131d5cff80e2981ea526280
Éric <nascimento3ric@gmail.com>
2023-01-08 01:11:43 -0300

    page login and remember password

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,34 @@
+import { Html } from 'next/document'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
-function Home(){
-    return <div className="title">Home do éric .-.</div>
+
+export default function Home() {
+    return(
+
+        <html lang='pt-br'>
+            <Head>
+                <title>Login sistema</title>
+            </Head>
+
+            <body>
+               <div>
+                   <div className='panel'>
+                       <h1 className='title'>Bem-vindo de volta!</h1>
+                       <h2 className='sub-title'>Insira suas informações de login para acessar o sistema</h2>
+                       <input type="text" placeholder='Login'></input>
+                       <input type="password" placeholder='Senha'></input>
+
+                       <Link href='/not-remember-password'>Esqueci minha senha</Link>
+                       <button className='btn-action'>Acessar</button>
+                   </div>
+               </div>
+            </body>
+
+        </html>
+
+     
+
+    )
 }
-
-export default Home
---
+++ src/pages/not-remember-password.js
@@ -0,0 +1,34 @@
+import { Html } from 'next/document'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Home() {
+    return(
+
+        <html lang='pt-br'>
+            <Head>
+                <title>Esqueci minha senha :/ </title>
+            </Head>
+
+            <body>
+               <div>
+                   <div className='panel'>
+                       <h1 className='title'>Esqueceu sua senha ?</h1>
+                       <h2 className='sub-title'>Digite uma senha que você lembra de ter utilizado</h2>
+                       <input type="password" placeholder='Já usei essa'></input>
+                       {/* <input type="password" placeholder='Senha'></input> */}
+
+                       <Link href='/' className='btn-secondary-action'>Voltar</Link>
+                       <button className='btn-action'>Tentar</button>
+                   </div>
+               </div>
+            </body>
+
+        </html>
+
+     
+
+    )
+}
