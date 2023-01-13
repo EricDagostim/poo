34b6276dbd7f649549aa5e603208471fdc32cb59
Éric <nascimento3ric@gmail.com>
2023-01-12 21:58:21 -0300

    tampo adicionado

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,53 @@
 
-import Link from 'next/link'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import { Html } from 'next/document'
+import Link from 'next/link';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Head from 'next/head';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 
 
 function Home() {
     return(
+        
 
-    
-                <div>
-                    <Link href='/not-remember-password'>
-                        Sim
-                    </Link>
-    
-                    <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            
-                    <Button variant="contained" color="success" size="small">
-                        Success
-                    </Button>
-                </div>
+        <>
+  
+            <Head>
+                <title>Página de login</title>
+            </Head>
+       
+        
+        
+        
+
+        <div class="div-login">
+          <TextField id="outlined-basic" label="E-mail" variant="outlined"/>
+          <TextField id="outlined-password-input" label="Password" variant="outlined" autoComplete="current-password" type="password"/>
+           
+             
+          <Link href='/not-remember-password'> 
+              Esqueci minha senha             
+          </Link>
+       
+          
+          <Button variant="contained" color="success" size="small">
+              Success
+          </Button>    
+
+
+          <Link href='/tempo'> 
+                <Button variant="contained" color="error" size="small">
+                Tempo
+                </Button>        
+          </Link>
+        </div>
+      
+
+        </>
+      
             
     )
 }
